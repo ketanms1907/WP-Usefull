9cbc2aae8367466028e2c950c10790704aa404aa
ketanms1907 <110393942+ketanms1907@users.noreply.github.com>
2022-08-01 17:54:07 +0530

    Create wordpress-usefull-link.php

---
+++ wordpress-usefull-link.php
@@ -0,0 +1,61 @@
+
+1.Create WP Themes
+
+http://underscores.me/
+https://github.com/devinsays/options-framework-theme
+
+2./*Custom post*/
+https://css-tricks.com/creating-meet-team-page-wordpress/
+https://css-tricks.com
+
+3.Post Type Generator
+https://generatewp.com/post-type/
+
+4.Convert html into wordpress : 
+https://htmltowordpress.io/
+
+5.Not display UPDATE plugin :
+define('DISALLOW_FILE_MODS',true);
+
+
+6.Share pc tp pc http://sharedrop.io
+
+7.photoshop https://www.photopea.com
+
+8.Check website https://builtwith.com/
+
+9.Wordpress Htaccess
+http://www.htaccessredirect.net/
+Wordpress All things http://vasashreya.blogspot.in/
+Wordpress All Schema generate https://technicalseo.com/seo-tools/schema-markup-generator/
+
+10.Page speed:
+https://www.keycdn.com/blog/google-pagespeed-insights-wordpress/
+https://varvy.com/pagespeed/
+
+Pagespeed Check :
+https://developers.google.com/speed/pagespeed/insights/
+https://gtmetrix.com/
+https://tools.pingdom.com/
+-----
+11.Redirect 404:
+http://www.htaccessredirect.net/
+http://www.rapidtables.com/web/tools/redirect-generator.htm
+https://www.austinjbay.com/how-to-redirect-20-blank-space-htaccess-file/
+https://simonecarletti.com/blog/2009/01/apache-rewriterule-and-query-string/
+
+12.https://github.com/devinsays/options-framework-theme
+
+13./*Custom post*/
+https://css-tricks.com/creating-meet-team-page-wordpress/
+https://css-tricks.com
+
+14.PostType Generator
+https://generatewp.com/post-type/
+
+15.Schema
+http://searchengineland.com/schema-markup-structured-data-seo-opportunities-site-type-231077
+http://www.bloggingspell.com/add-schema-org-markup-wordpress/
+http://keithdevon.com/schema-on-your-wordpress-website/
+https://yandex.com/support/webmaster/supported-schemas/questions.xml
+https://gtmetrix.com/
